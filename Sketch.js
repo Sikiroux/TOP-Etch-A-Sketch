2771e3b0d93ca2c00a90fffe5ed359f7cd0c2b14
Damien Ekouma <damienekouma@gmail.com>
2022-11-10 14:35:54 -1000

    Add hovering effect and ending on mouse leave

--- Sketch.js
+++ Sketch.js
@@ -1,11 +1,19 @@
-//container for all the divs incoming
+//container for all the divs incoming;
 const divContainer = document.createElement("div");
 
 divContainer.classList.add("container");
 document.body.appendChild(divContainer);
 
+
+//create all the divs;
 for (i = 0; i < 256; i++) {
+    //create the div;
     let div = document.createElement("div");
+    //add the div inside the container;
     divContainer.appendChild(div);
     div.classList.add("grid");
+    //add a hovering effect;
+    div.onmouseover = () => div.classList.add("hover")
+    div.onmouseleave = () => div.classList.remove("hover")
 }
+
