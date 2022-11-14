c4ee0bc1640f297a2cd79986c081d6ba317d96f7
Damien Ekouma <damienekouma@gmail.com>
2022-11-14 02:53:36 -1000

    Add the functionality so that the grid never change width and height; With minmax(0; 1fr);

--- Sketch.js
+++ Sketch.js
@@ -8,17 +8,24 @@ document.body.appendChild(divContainer);
 let inputBtnValue =  0;
 let x = 0;
 
+//creation of the button;
 const btn = document.querySelector("button");
 btn.addEventListener("click", () => {
-    inputBtnValue = Number(prompt("Choisi une valeur :", "O"));
+    inputBtnValue = Number(prompt("Choisi une valeur inférieur ou égale a 64 :", "O"));
+    if (inputBtnValue > 64) {
+        alert("ERROR, choose a number equal or under 64, words are not accepted!!!");
+        return;
+    }
     x = inputBtnValue;
 });
+
+//change the size of the grid
 btn.addEventListener("click", () => {
-    divContainer.style.gridTemplateColumns = `repeat(${x}, 20px)`;
-    divContainer.style.gridTemplateRows = `repeat(${x}, 20px)`;
+    divContainer.style.gridTemplateColumns = `repeat(${x}, minmax(0, 1fr))`;
+    divContainer.style.gridTemplateRows = `repeat(${x}, minmax(0, 1fr))`;
 })
 
-
+//Base 16 * 16 px
 for (i = 0; i < 256; i++) {
     //create the div;
     let div = document.createElement("div");
@@ -35,18 +42,16 @@ for (i = 0; i < 256; i++) {
 
 
 
-//create all the divs;
+//Change the px * px;
 btn.addEventListener("click", () => {
     for (i = 0; i < x * x; i++) {
-    //create the div;
-    let div = document.createElement("div");
-    //add the div inside the container;
-    divContainer.appendChild(div);
-    div.classList.add("grid");
-    //add a hovering effect;
-    div.onmouseover = () => div.classList.add("hover")
-    div.onmouseleave = () => div.classList.remove("hover")
-}
+        //create the div;
+        let div = document.createElement("div");
+        //add the div inside the container;
+        divContainer.appendChild(div);
+        div.classList.add("grid");
+        //add a hovering effect;
+        div.onmouseover = () => div.classList.add("hover");
+        div.onmouseleave = () => div.classList.remove("hover");
+    }
 })
-
-
